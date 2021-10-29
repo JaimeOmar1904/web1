986a080a9f49f23315e7d4b3d963f109680649c4
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2021-10-28 21:32:50 -0500

    Los recursos de mi pagina

---
+++ Recursos/saludo.java
@@ -0,0 +1,10 @@
+public class void{
+
+    public static void main(String args []){
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        System.out.println("Me llamo jaime y tu");
+        nombre = entrada.nextString();
+    }
+
+}
